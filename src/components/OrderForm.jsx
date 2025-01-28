0d1afc696462699f3218cda917517d65f05a6524
Latif <latif_1995@live.no>
2025-01-28 09:59:31 +0100

    form inputs ready

--- src/components/OrderForm.jsx
+++ src/components/OrderForm.jsx
@@ -1,26 +1,57 @@
+import React from "react";
+
 export default function OrderForm() {
-  console.log("hello from from order form");
+  let [status, setStatus] = React.useState("");
+
+  function handleStatus(text) {
+    setStatus((status = text));
+  }
+
+  function handleOrder(event) {
+    event.preventDefault();
+
+    const formData = new FormData(event.target);
+
+    console.log(formData);
+
+    const order = {
+      fullname: formData.get("fullname"),
+      email: formData.get("email"),
+      phone: formData.get("phone"),
+      address: formData.get("address"),
+      premeal: formData.get("premeal"),
+      main: formData.get("main"),
+      drikke: formData.get("drikke"),
+      dessert: formData.get("dessert"),
+      time: formData.get("time"),
+      dateInput: formData.get("date-input"),
+      status: formData.get("status"),
+    };
+
+    console.log(order);
+  }
+
   return (
     <section className="bg-amber-200">
       <button className="close">Close Order Form</button>
-      <form className="form">
+      <form className="form" onSubmit={handleOrder}>
         <div className="person">
-          <label htmlFor="">Name:</label>
-          <input type="text" />
+          <label htmlFor="fullname">Full name:</label>
+          <input name="fullname" placeholder="name" type="text" />
 
-          <label htmlFor="">Email:</label>
-          <input type="email" />
+          <label htmlFor="email">Email:</label>
+          <input name="email" placeholder="email" type="email" />
 
-          <label htmlFor="">Phone:</label>
-          <input type="tel" />
+          <label htmlFor="phone">Phone:</label>
+          <input name="phone" placeholder="number" type="tel" />
 
-          <label htmlFor="">Address:</label>
-          <input type="text" />
+          <label htmlFor="address">Address:</label>
+          <input name="address" placeholder="address" type="text" />
         </div>
 
         <div className="order">
-          <label htmlFor="">Forrett:</label>
-          <select name="" id="">
+          <label htmlFor="premeal">PreMeal:</label>
+          <select name="premeal" id="preMeal">
             <option value="bruschetta">Bruschetta</option>
             <option value="tomatsuppe">Tomatsuppe</option>
             <option value="caprese">Caprese</option>
@@ -30,12 +61,84 @@ export default function OrderForm() {
             <option value="rakfisk">Rakfisk</option>
           </select>
 
-          <label htmlFor="">Hovedrett:</label>
-          <select name="" id="">
+          <label htmlFor="main">Main course</label>
+          <select name="main" id="main">
             <option value="france">France</option>
             <option value="south-korea">South Korea</option>
+            <option value="india">India</option>
+            <option value="japan">Japan</option>
+            <option value="usa">USA</option>
+            <option value="england">England</option>
+            <option value="china">China</option>
           </select>
+
+          <label htmlFor="drink">Drikke</label>
+          <select name="drikke" id="drink">
+            <option value="pepsi-max">pepsi max</option>
+            <option value="coca-cola">coca-cola</option>
+            <option value="sprite">sprite</option>
+            <option value="milshake">Milkshake</option>
+            <option value="lemonade">lemonade</option>
+          </select>
+
+          <label htmlFor="dessert">Dessert</label>
+          <select name="dessert" id="desert">
+            <option value="sjokolade is">sjokolade is</option>
+            <option value="Cake">Cake</option>
+            <option value="Ananas cake">Ananas cake</option>
+            <option value="milshake">Milkshake mountain</option>
+            <option value="kunafah">kunafah</option>
+          </select>
+
+          <label htmlFor="notat">Tillegg, side, Notater</label>
+          <textarea
+            name="notat"
+            id="notat"
+            placeholder="tilleggs notater, allergi"
+          ></textarea>
         </div>
+
+        <div className="flex justify-around">
+          <label htmlFor="status" className="text-2xl">
+            Pick up
+          </label>
+          <input
+            name="status"
+            className="h-4 w-14"
+            type="radio"
+            value="pick up"
+            onChange={() => handleStatus("Pick up")}
+          />
+          <label htmlFor="status" className="text-2xl">
+            Delivery
+          </label>
+          <input
+            name="status"
+            className="h-4 w-14"
+            type="radio"
+            value="delivery"
+            id="status"
+            onChange={() => handleStatus("Delivery")}
+          />
+        </div>
+        <labe htmlFor="time" className="text-2xl">
+          Time for{" "}
+          <p
+            className={status === "Pick up" ? "bg-green-400" : "bg-orange-400"}
+          >
+            {" "}
+            {status}
+          </p>{" "}
+        </labe>
+        <input type="time" name="time" value="13:30" />
+        <input type="date" name="date-input" value="2049-04-12" />
+
+        <button
+          className=" bg-green-400 h-14 rounded-2xl text-2xl"
+          type="submit"
+        >
+          Order
+        </button>
       </form>
     </section>
   );
